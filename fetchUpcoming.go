27ad05b44c1fefa52e9f8aebde51cffb04e5349b
hannasamuel20 <hanna17st@gmail.com>
2019-12-11 23:46:47 +0300

    data displayed on an html file

--- fetchUpcoming.go
+++ fetchUpcoming.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+
 	"net/http"
 )
 
@@ -11,8 +13,11 @@ type response struct {
 	MovieList []results `json:"results"`
 }
 type results struct {
-	Title    string `json:"title"`
-	Overview string `json:"overview"`
+	Title       string `json:"title"`
+	PosterPath  string `json:"poster_path"`
+	Overview    string `json:"overview"`
+	Id          int    `json:"id"`
+	ReleaseDate string `json:"release_date"`
 }
 
 func getUpcomingMovies(body []byte) (*response, error) {
@@ -28,8 +33,16 @@ func getUpcomingMovies(body []byte) (*response, error) {
 
 const b = "https://api.themoviedb.org/3/movie/upcoming?"
 const apiKey = "f4b8e415cb9ab402e5c1d72176cab35b"
+const image = "http://image.tmdb.org/t/p/w500/pjeMs3yqRmFL3giJy4PMXWZTTPa.jpg"
+const path = "/pjeMs3yqRmFL3giJy4PMXWZTTPa.jpg"
 
-func main() {
+var temp *template.Template
+
+func init() {
+	temp = template.Must(template.ParseGlob("*.html"))
+}
+
+func upcoming_handler(w http.ResponseWriter, r *http.Request) {
 	res, err := http.Get(b + "api_key=" + apiKey)
 	if err != nil {
 		panic(err.Error())
@@ -41,5 +54,12 @@ func main() {
 	}
 	s, err := getUpcomingMovies([]byte(body))
 	fmt.Println(s)
+	temp.ExecuteTemplate(w, "display.html", s)
+
+}
+
+func main() {
+	http.HandleFunc("/", upcoming_handler)
+	http.ListenAndServe(":8080", nil)
 
 }
