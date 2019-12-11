6d2f960ac54221affed949aa4c0d4a3f4d665985
Adona Tesfaye <adonatesfaye99@gmail.com>
2019-12-12 02:39:39 +0300

    change

--- Movie.go
+++ Movie.go
@@ -1,20 +1,27 @@
 package main
 
-import ("net/http"
-		 "html/template")
-var temple=template.Must(template.ParseFiles("index.html"))
-func index(w http.ResponseWriter, r *http.Request){
-temple.Execute(w,nil)
+import (
+	"html/template"
+	"net/http"
+)
+
+var temple = template.Must(template.ParseFiles("index.html"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	temple.Execute(w, nil)
 }
-func main(){
-	mux:=http.NewServeMux()
-	style:=http.FileServer(http.Dir("css/"))
-	image:=http.FileServer(http.Dir("image/"))
-	js:=http.FileServer(http.Dir("js/"))
-	mux.Handle("/js/",http.StripPrefix("/js/",js))
-	mux.Handle("/css/",http.StripPrefix("/css/",style))
-	mux.Handle("/image/",http.StripPrefix("/image/",image))
-	mux.HandleFunc("/",index)
-	http.ListenAndServe(":8080",mux)
+func index(w http.ResponseWriter, r *http.Request) {
+	temple.Execute(w, nil)
+}
+func main() {
+	mux := http.NewServeMux()
+	style := http.FileServer(http.Dir("css/"))
+	image := http.FileServer(http.Dir("image/"))
+	js := http.FileServer(http.Dir("js/"))
+	mux.Handle("/js/", http.StripPrefix("/js/", js))
+	mux.Handle("/css/", http.StripPrefix("/css/", style))
+	mux.Handle("/image/", http.StripPrefix("/image/", image))
+	mux.HandleFunc("/", index)
+	http.ListenAndServe(":8080", mux)
 
 }
