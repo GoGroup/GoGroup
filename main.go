97888d9eb8b581a6e937cacf502884bb5bc571a6
Adona Tesfaye <adonatesfaye99@gmail.com>
2019-12-15 03:18:19 +0300

    display movies added

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseGlob("view/template/*"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	tmpl.ExecuteTemplate(w, "index.layout", nil)
+}
+func display(w http.ResponseWriter, r *http.Request) {
+
+	tmpl.ExecuteTemplate(w, "Movie.layout", nil)
+}
+func main() {
+	fs := http.FileServer(http.Dir("view/assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/Movie", display)
+	http.ListenAndServe(":8181", nil)
+}
