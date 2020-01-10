acdc15438017ded7319b7b45c380f8e52df357ba
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-01-10 08:17:26 +0300

    Api for cinema

--- model/Schedule.go
+++ model/Schedule.go
@@ -2,13 +2,13 @@ package model
 
 type Schedule struct {
 	ID           uint   `json: "id" gorm:"primary_key;AUTO_INCREMENT"`
-	MoviezID     int    `json: "moviezid"`
+	MoviemID     int    `json: "moviemid"`
 	StartingTime string `json: "startingtime" gorm:"type:varchar(255);not null"`
 	Dimension    string `json: "dimension" gorm:"type:varchar(255);not null"`
 	HallID       int    `json: "hallid"`
 	Day          string `json: "day" gorm:"type:varchar(255);not null"`
 }
-type Moviez struct {
+type Moviem struct {
 	ID   uint `json: "id" gorm:"primary_key;AUTO_INCREMENT"`
 	Tmdb int  `json: "tmdb"`
 }
