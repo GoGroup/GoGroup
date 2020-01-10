63737a2c036bce622c3090a6a3df586035f85a06
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-01-10 05:11:36 +0300

    cinema API POST ADDED

--- model/hall.go
+++ model/hall.go
@@ -1,10 +1,10 @@
 package model
 
 type Hall struct {
-	ID        uint   `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
-	HallName  string `json:"hallname" gorm:"type:varchar(255);not null"`
-	Capacity  uint   `json:"capacity"`
-	CinemaIDY uint   `json:"cinemaid"`
+	ID       uint   `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
+	HallName string `json:"hallname" gorm:"type:varchar(255);not null"`
+	Capacity uint   `json:"capacity"`
+	CinemaID uint   `json:"cinemaid"`
 
 	Price           uint `json:"Price"`
 	VIPPrice        uint `json:"vipprice"`
