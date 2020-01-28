1af99e81373a2e98f3864ccbdea9c6cb0e20634e
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-01-28 22:17:45 +0300

    menu handler

--- cinev_park/http/handler/admin_handler_test.go
+++ cinev_park/http/handler/admin_handler_test.go
@@ -144,45 +144,7 @@ func TestAdminEvent(t *testing.T) {
 
 }
 
-// func TestAdminSchedule(t *testing.T) {
 
-// 	tmpl := template.Must(template.ParseGlob("../../../view/template/*"))
-
-// 	schdlRepo := schrep.NewMockScheduleRepo(nil)
-// 	schdlServ := schser.NewScheduleService(schdlRepo)
-
-// 	adminEvHandler := NewAdminHandler(tmpl, nil, nil, schdlServ, nil, nil, nil)
-
-// 	mux := http.NewServeMux()
-// 	mux.HandleFunc("/admin/cinemas/schedule/1", adminEvHandler.AdminSchedule)
-// 	ts := httptest.NewTLSServer(mux)
-// 	defer ts.Close()
-
-// 	tc := ts.Client()
-// 	URL := ts.URL
-
-// 	resp, err := tc.Get(URL + "/admin/cinemas/schedule/")
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		t.Errorf("want %d, got %d", http.StatusOK, resp.StatusCode)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	if !bytes.Contains(body, []byte("Monday")) {
-// 		t.Errorf("want body to contain %q", body)
-// 	}
-
-// }
 
 func TestAdminEventDelete(t *testing.T) {
 
@@ -220,44 +182,6 @@ func TestAdminEventDelete(t *testing.T) {
 
 }
 
-// func TestBookingNew(t *testing.T) {
-
-// 	tmpl := template.Must(template.ParseGlob("../../../view/template/*"))
-
-// 	bookepo := bks.NewMockBookingepo(nil)
-// 	bookServ := bkr.NewBookingService(bookepo)
-
-// 	MenuHandler := NewMenuHandler(tmpl, nil, nil, nil, nil,nil,nil,nil ,bookServ)
-
-// 	mux := http.NewServeMux()
-// 	mux.HandleFunc("/admin/cinemas/events/new/", MenuHandler.)
-// 	ts := httptest.NewTLSServer(mux)
-// 	defer ts.Close()
-
-// 	tc := ts.Client()
-// 	URL := ts.URL
-// 	csrfSignKey := []byte(rtoken.GenerateRandomID(32))
-// 	form := url.Values{}
-// 	form.Add("name", model.EvenMock.Name)
-// 	form.Add("description", model.EvenMock.Description)
-// 	form.Add("time", model.EvenMock.Time)
-// 	form.Add("image", model.EvenMock.Image)
-// 	form.Add("location", model.EvenMock.Location)
-// 	CSFRToken, _ := rtoken.GenerateCSRFToken(csrfSignKey)
-// 	form.Add(csrfHKey, CSFRToken)
-
-// 	resp, err := tc.PostForm(URL+"/admin/cinemas/events/new/", form)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		t.Errorf("want %d, got %d", http.StatusOK, resp.StatusCode)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// }
 func TestAdminEventUpdate(t *testing.T) {
 
 	tmpl := template.Must(template.ParseGlob("../../../view/template/*"))
